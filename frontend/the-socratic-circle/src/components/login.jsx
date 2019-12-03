ad3cac32528c680d81c3427492b053fdca3e40e9
Automage <pranav92000@gmail.com>
2019-12-03 03:40:46 -0500

    Setup login for auth

--- frontend/the-socratic-circle/src/components/login.jsx
+++ frontend/the-socratic-circle/src/components/login.jsx
@@ -1,26 +1,72 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 class Login extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: "",
+    loading: false,
+    loggedIn: false,
+    error: ""
+  };
 
-  handleSubmit = event => {
-    console.log(event);
+  componentDidMount() {
+    let token = localStorage.getItem("jwtToken");
+    if (token) {
+      console.log("token", token);
+      const decoded = jwt_decode(token);
+      if (decoded) {
+        this.setState({ loggedIn: true });
+      }
+    }
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    let userData = { email: this.state.email, password: this.state.password };
+
+    try {
+      const res = await axios.post("/api/login", userData);
+
+      // Save to localStorage
+      // Set token to localStorage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+
+      this.setState({ loading: false, loggedIn: true });
+    } catch (err) {
+      console.log(err);
+      this.setState({ loading: false, error: "Login Failed" });
+    }
   };
 
   render() {
+    if (this.state.loggedIn === true) {
+      return <Redirect to="/home" />;
+    }
+
     return (
       <div className="container-fluid form-container">
         <div className="card form view-component">
           <h3 className="text-center">Login</h3>
           <form onSubmit={this.handleSubmit}>
             <div className="form-group">
-              <label className="form-label">Username</label>
+              <label className="form-label">Email</label>
               <input
-                type="text"
+                type="email"
                 className="form-control"
-                id="username"
-                placeholder="Username"
+                id="email"
+                value={this.state.email}
+                placeholder="Enter email"
+                onChange={this.handleChange("email")}
               />
             </div>
             <div className="form-group">
@@ -28,8 +74,10 @@ class Login extends Component {
               <input
                 type="password"
                 className="form-control"
+                value={this.state.password}
                 id="exampleInputPassword1"
                 placeholder="Password"
+                onChange={this.handleChange("password")}
               />
             </div>
             <div className="row">
@@ -47,6 +95,11 @@ class Login extends Component {
               </div>
             </div>
           </form>
+          {this.state.error && (
+            <div class="alert alert-primary form-alert" role="alert">
+              {this.state.error}
+            </div>
+          )}
         </div>
       </div>
     );
