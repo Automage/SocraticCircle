215401f40ebc1c52b184ad5341502d23dc40fb37
Automage <pranav92000@gmail.com>
2019-12-03 03:48:10 -0500

    setup register for auth

--- frontend/the-socratic-circle/src/components/register.jsx
+++ frontend/the-socratic-circle/src/components/register.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 class Register extends Component {
-  state = {};
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+    loading: false,
+    error: ""
+  };
 
-  handleSubmit = event => {
-    console.log(event);
+  componentDidMount() {
+    let token = localStorage.getItem("jwtToken");
+    if (token) {
+      console.log("token", token);
+      console.log(token);
+      const decoded = jwt_decode(token);
+      if (decoded) {
+        this.setState({ loggedIn: true });
+      }
+    }
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    if (this.state.password != this.state.password2) {
+      this.setState({ loading: false, msg: "Passwords don't match" });
+    } else {
+      let userData = {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password
+      };
+
+      try {
+        const res = await axios.post("/api/register", userData);
+
+        this.setState({ loading: false, msg: "Registration Successful." });
+      } catch (err) {
+        console.log(err);
+        this.setState({ loading: false, msg: "Registration Failed." });
+      }
+    }
   };
 
   render() {
@@ -21,6 +66,8 @@ class Register extends Component {
                 className="form-control"
                 id="username"
                 placeholder="Username"
+                value={this.state.username}
+                onChange={this.handleChange("username")}
               />
             </div>
             <div className="form-group">
@@ -31,6 +78,8 @@ class Register extends Component {
                 id="email"
                 aria-describedby="emailHelp"
                 placeholder="Enter email"
+                value={this.state.email}
+                onChange={this.handleChange("email")}
               />
               <small id="emailHelp" className="form-text text-muted">
                 We'll never share your email with anyone else.
@@ -43,6 +92,19 @@ class Register extends Component {
                 className="form-control"
                 id="password"
                 placeholder="Password"
+                value={this.state.password}
+                onChange={this.handleChange("password")}
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password2"
+                placeholder="Password"
+                value={this.state.password2}
+                onChange={this.handleChange("password2")}
               />
             </div>
             <div className="row">
@@ -60,6 +122,11 @@ class Register extends Component {
               </div>
             </div>
           </form>
+          {this.state.error && (
+            <div class="alert alert-primary form-alert" role="alert">
+              {this.state.error}
+            </div>
+          )}
         </div>
       </div>
     );
