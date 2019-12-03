6710047f5da53dc77c1027d0e545a9544df85501
Automage <pranav92000@gmail.com>
2019-12-03 02:40:58 -0500

    Setup dependencies for auth

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,29 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const apiRouter = require("./routes/api");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const passport = require("passport");
 
+const apiRouter = require("./routes/api");
 const app = express();
 
 // MongoDB connection
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("connected to database"));
 
 // Routing
+
 app.use(bodyParser.json());
 app.use("/", express.static("../frontend/the-socratic-circle/build"));
 app.use("/api", apiRouter);
 // Handle any other requests (could change to specify /login and /register)
 // For now there is only a single point of entry - index.html
 app.get("*", (req, res) => {
-  console.log(
-    path.join(__dirname + "/../frontend/the-socratic-circle/build/index.html")
-  );
   res.sendFile(
     path.join(__dirname + "/../frontend/the-socratic-circle/build/index.html")
   );
