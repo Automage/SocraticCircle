d21e105ce97702375c2e2078170dd099a512ba39
Automage <pranav92000@gmail.com>
2019-12-05 16:59:16 -0500

    Actually added build to repo

---
+++ backend/frontend/the-socratic-circle/build/precache-manifest.69399fcc00ce1e11859878add48a748b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44a20552311cd24352fc74234efff49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae443c4b4a8aa951ca28",
+    "url": "/static/css/main.efad390e.chunk.css"
+  },
+  {
+    "revision": "4a4b243155439fa53a53",
+    "url": "/static/js/2.b078dd1d.chunk.js"
+  },
+  {
+    "revision": "ae443c4b4a8aa951ca28",
+    "url": "/static/js/main.6db323d5.chunk.js"
+  },
+  {
+    "revision": "f60b284ecd8b17801b5d",
+    "url": "/static/js/runtime-main.c214d0db.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
